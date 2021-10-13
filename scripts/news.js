fccd364f5840787d86e5f7a7018818b2f55213fb
KAMZ13 <84303083+KAMZ13@users.noreply.github.com>
2021-10-13 00:13:55 -0500

    2nd update

--- scripts/news.js
+++ scripts/news.js
@@ -1,6 +1,8 @@
+const key = process.env.APIK;
 export class News {
     getAll(nodes) {
-        let url = 'https://newsapi.org/v2/everything?q=' + nodes + '&apiKey=9dd678c7876f4b49bf68e9d8508b6e22';
+        let url = `https://newsapi.org/v2/everything?q=${nodes}&apiKey=${key}`;
         return axios.get(url);
     }
 }
+;
